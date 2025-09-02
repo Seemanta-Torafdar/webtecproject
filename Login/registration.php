4dbafed259b607ea00a661676e1de43ce3067fd4
Seemanta Torafdar <seemanta11189@gmail.com>
2025-09-02 23:43:55 +0600

    add student registration ,teacher ragistration edit form

--- Login/registration.php
+++ Login/registration.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <title>Choose Registration</title>
- 
+
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
- <link rel="stylesheet" href="../CSS/registration.css">
+  <link rel="stylesheet" href="../CSS/registration.css">
 </head>
+
 <body>
   <div class="container">
     <!-- Logo -->
@@ -16,9 +18,12 @@
     <!-- Registration As box with buttons inside -->
     <div class="title-box">
       <h2>Registration As</h2>
-      <button class="choice-btn teacher"><i class="fa-solid fa-person-chalkboard"></i> Teacher</button>
-      <button class="choice-btn student"><i class="bi bi-mortarboard"></i> Student</button>
+      <button class="choice-btn teacher"><i class="fa-solid fa-person-chalkboard"></i><a href="../Login/regs.php">Teacher</button>
+
+      <button class="choice-btn student"><i class="bi bi-mortarboard"></i><a href="../Login/regs.php"> Student</button>
+
     </div>
   </div>
 </body>
+
 </html>
--- Login/regs.php
+++ Login/regs.php
@@ -18,10 +18,10 @@
             <!-- Age -->
             <label for="age">Age</label>
             <input type="number" id="age" name="age" required placeholder="Enter Age">
-            <!-- Date Of Birth -->
+             <!-- Date Of Birth -->
             <label for="birth">Date of Birth</label>
             <input type="date" id="birth" name="birth" required >
-            
+
             <!-- Phone Number -->
             <label for="phone">Phone Number</label>
             <input type="text" id="phone" name="phone" required placeholder="(+880) 17456-78901">
@@ -30,6 +30,20 @@
             <label for="email">Email Address</label>
             <input type="email" id="email" name="email" required placeholder="User email address">
 
+            <!-- Class -->
+            <label for="class">Class Name</label>
+            <input type="text" id="class" name="class" required>
+
+            <!-- Section -->
+            <label for="section">Section</label>
+            <select id="section" name="section" required>
+                <option value="">Select Section</option>
+                <option value="sectionA">Section A</option>
+                <option value="sectionB">Section B</option>
+                <option value="sectionC">Section C</option>
+            </select>
+
+           
 
             <!-- Password -->
             <label for="password">Password</label>
